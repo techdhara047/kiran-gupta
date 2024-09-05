d6b57d1e37e8d1e2d0c1d601aba8db08426230b3
techdhara047 <techdhara047@gmail.com>
2024-09-06 03:29:01 +0530

    addded sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), react()]
+  site: "https://kiran-gupta.netlify.app/",
+  integrations: [icon(), sitemap()],
 });
